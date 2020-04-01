4f669f075a936d5a2a95b49e419b30f4f7596038
shivam7374 <shivamsingla491@gmail.com>
2020-04-01 14:33:04 +0530

    ADD FILE

--- SUDOKU UPDATED.cpp
+++ SUDOKU UPDATED.cpp
@@ -98,6 +98,16 @@ int main()
         }
 
     }
+    cout<<endl<<"This is what you entered"<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++)
+        {
+            cout<<mat[i][j]<<" ";
+        }
+        cout<<endl;
+
+    }
+    
 	sudokusolver(mat,0,0,n);
 	return 0;
 }
